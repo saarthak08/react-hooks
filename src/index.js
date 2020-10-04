f86ee6d0cb498fddae939a0cbaffeb8f7bfdbedd
Saarthak Gupta <saarthakgupta08@gmail.com>
2020-10-04 12:31:42 +0530

    add useState sample

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 
+const App = (props) => {
+  const [count, setCount] = useState(props.count);
+  const [text,setText] = useState(props.text);
+
+  return (
+    <div>
+      <p>The current {text || 'count'} : {count}</p>
+      <button onClick={()=>setCount(count + 1)}>+1</button>&nbsp;
+      <button onClick={()=>setCount(count - 1)}>-1</button>&nbsp;
+      <button onClick={()=>setCount(0)}>Reset</button>&nbsp;
+      <input type='text' onChange={(e)=>setText(e.target.value)}></input>
+    </div>
+  );
+};
+
+App.defaultProps = {
+  count:0,
+  text:''
+};
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <div>Hello World!</div>
+    <App count={2}/>
   </React.StrictMode>,
   document.getElementById("root")
 );
